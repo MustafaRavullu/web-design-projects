c81f2ae1663413c460b19db82e480aada43df4b0
MustafaRavullu <muhammedravullu@gmail.com>
2024-09-17 19:43:51 +0300

    feat: change icon when megamenu appears

--- projects/ableton-about/scripts.js
+++ projects/ableton-about/scripts.js
@@ -1,6 +1,14 @@
 const headerNavButton = document.getElementById("headerNavButton");
 const headerMegaMenu = document.getElementById("header__mega-menu");
+const headerNavButtonIconPlus = document.getElementById(
+  "header__nav-button__icon-plus"
+);
+const headerNavButtonIconMinus = document.getElementById(
+  "header__nav-button__icon-minus"
+);
 
 headerNavButton.addEventListener("click", function () {
   headerMegaMenu.classList.toggle("show-mega-menu");
+  headerNavButtonIconPlus.classList.toggle("hide");
+  headerNavButtonIconMinus.classList.toggle("hide");
 });
